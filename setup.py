853db6064f49c92b9321155557f358c1f526d0a1
Developer <devel@TOM-Development.(none)>
2011-09-28 03:38:02 +0200

    added -msse2 compiler argument

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from Cython.Distutils import build_ext
 
 setup(
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("pyback", ["pyback.pyx"])]
+    ext_modules = [Extension("pyback", ["pyback.pyx"],extra_compile_args=['-msse2'])]
 )
